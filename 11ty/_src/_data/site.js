ce2174917c6c39f22de392d93e7f4234aa7be3d1
Andre A Alves <andre@alves.me>
2023-05-01 10:38:34 +0200

    Added footer text

--- 11ty/_src/_data/site.js
+++ 11ty/_src/_data/site.js
@@ -1,6 +1,7 @@
 module.exports = {
     meta: {
         title: "Qiskit Sample Test Answers",
+        author: "Andre A Alves",
         lang: "en-us",
         description: "Answers and explanations for the sample questions for the Qiskit developer test"
     }
