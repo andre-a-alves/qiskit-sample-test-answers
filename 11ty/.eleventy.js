0cf062e8075753a186a23d28b2f9ad3dc67e245a
Andre A Alves <andre@alves.me>
2023-05-05 05:20:45 +0200

    Repairs to prepend

--- 11ty/.eleventy.js
+++ 11ty/.eleventy.js
@@ -18,12 +18,13 @@ module.exports = function (eleventyConfig) {
     })
 
     return {
-        markdownTemplateEngine: "njk",
-        htmlTemplateEngine: "njk",
         dir: {
             input: "_src",
             output: "_site",
             includes: "_includes",
-        }
+        },
+        htmlTemplateEngine: "njk",
+        markdownTemplateEngine: "njk",
+        pathPrefix: "/qiskit-sample-test-answers/",
     }
 }
